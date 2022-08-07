e5faf78ab0ae80cb28ea37101b81c9bed6cd5806
RichardLeonardoSavage <savagerichard100@gmail.com>
2022-08-07 13:31:16 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace LearningReturnStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //here we are calling the method and returning a value back to the main method
+            int cubedNumber = cube(3);
+            Console.WriteLine(cubedNumber);
+
+
+            Console.ReadLine();
+
+        }
+
+        static int cube(int num)
+        {
+            //in this method we taking the value of of the variable cube and telling it to num * num * num and return the result
+            int result = num * num * num;
+            return result;
+        }
+    }
+}
